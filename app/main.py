83434777ab4052badceccac26b9fbedad53d77c6
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-04-05 10:24:33 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ import os
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        
+
     def __enter__(self) -> None:
         return self
-        
+
     def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
         os.remove(self.filename)
-    
